e2762ce69f39db9daa0716467be5b155cd56fec2
mohanad <mohanadhessen@gmail.com>
2026-09-02 14:42:58 +0300

    Update Header, Main layout and add BestSellerCards component

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,31 +1,15 @@
 import styles from "./Header.module.css";
 
 function Header() {
-    let notificationIcon = (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-mail-icon lucide-mail"
-        >
-            <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
-            <rect x="2" y="4" width="20" height="16" rx="2" />
-        </svg>
-    );
-
+    let notificationIcon = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bell-icon lucide-bell"><path d="M10.268 21a2 2 0 0 0 3.464 0" /><path d="M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326" /></svg>
+    let activeNotificationIcon = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bell-dot-icon lucide-bell-dot"><path d="M10.268 21a2 2 0 0 0 3.464 0" /><path d="M11.68 2.009A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673c-.824-.85-1.678-1.731-2.21-3.348" /><circle cx="18" cy="5" r="3" /></svg>
     let plusIcon = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14" /><path d="M12 5v14" /></svg>
 
     return (
         <header className={styles.header}>
             <div className={styles.notificationContainer}>
                 <button
-                    className={styles.icon}
+                    className={`${styles.icon} ${styles.notification}`}
                     id="notificationIcon"
                     aria-label="Notifications"
                 >
--- src/layouts/Main/Main.jsx
+++ src/layouts/Main/Main.jsx
@@ -3,8 +3,7 @@ import Customers from "../../components/cards/Customers/Customers";
 import Shipment from "../../components/cards/Shipment/Shipment";
 import Revenue from "../../components/cards/Revenue/Revenue";
 import CashOnDelivery from "../../components/cards/CashOnDelivery/CashOnDelivery";
-import AvrageOrderValue from "../../components/cards/AvrageOrderValue/AvrageOrderValue";
-
+import BestSellerCards from "../../components/cards/BestSellerCards/BestSellerCards";
 
 function Main() {
   return (
@@ -15,19 +14,33 @@ function Main() {
         <CashOnDelivery />
         <CashOnDelivery />
         <CashOnDelivery />
-
       </div>
       <div className={styles.mainContiner}>
         <div className={styles.leftContiner}>
+          <div className={styles.orderActions}>
+          
+          </div>
+          
+          <div className={styles.orderSection}>
+            <ul>
+              <li>Order ID</li>
+              <li>Customer </li>
+              <li>Number </li>
+              <li>Status</li>
+              <li>Shipping Method</li>
+              <li>Date</li>
+              <li>Total</li>
+              <li>Number of Items</li>
+            </ul>
+            <div className={styles.divider}></div>
+          </div>
 
         </div>
         <div className={styles.rightContiner}>
-
           <Shipment />
-          <div className={styles.bestSellers}></div>
+          <BestSellerCards />
         </div>
       </div>
-
     </main>
   );
 }
