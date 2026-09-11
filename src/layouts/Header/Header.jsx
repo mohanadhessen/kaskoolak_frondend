be1e24a279837987bc5490eeb61935c200918b5f
Mohanad Hessen <mohanadhessen@gmail.com>
2026-09-11 20:06:05 +0300

    added the icon

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -5,7 +5,7 @@ import React, { useState } from "react"
 function Header() {
 
     const [showModal, setShowModal] = useState(false)
-
+    let userIcon = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" /><circle cx="12" cy="7" r="4" /></svg>
     let notificationIcon = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bell-icon lucide-bell"><path d="M10.268 21a2 2 0 0 0 3.464 0" /><path d="M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326" /></svg>
     let activeNotificationIcon = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bell-dot-icon lucide-bell-dot"><path d="M10.268 21a2 2 0 0 0 3.464 0" /><path d="M11.68 2.009A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673c-.824-.85-1.678-1.731-2.21-3.348" /><circle cx="18" cy="5" r="3" /></svg>
     let plusIcon = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14" /><path d="M12 5v14" /></svg>
@@ -32,11 +32,15 @@ function Header() {
                             <div className={styles.createOrderBody}>
 
                                 <div className={styles.customerInfo}>
-                                    <input
-                                        className={styles.customerName}
-                                        type="text"
-                                        placeholder="Customer name"
-                                    />
+                                    <div className={styles.customerNameContainer}>
+                                        {userIcon}
+
+                                        <input
+                                            className={styles.customerName}
+                                            type="text"
+                                            placeholder="Customer name"
+                                        />
+                                    </div>
 
                                     <input
                                         type="tel"
@@ -65,7 +69,7 @@ function Header() {
                                 />
                                 <div className={styles.courierGroup}>
                                     <div className={styles.courierField}>
-                                        
+
                                         <select id="courier" name="courier">
                                             <option value="">Select courier</option>
                                             <option value="bosta">Bosta</option>
@@ -96,7 +100,7 @@ function Header() {
                                     </div>
                                 </div>
 
-                                <div className={styles.customerInfo}>
+                                <div className={styles.orderPricing}>
                                     <input
                                         type="number"
                                         placeholder="Shipping cost"
